57084913e62845ee8bd08522fc2ce9989e813393
AbscodInformatics <shivanshupathak@Shivanshus-MacBook-Air.local>
2025-05-15 11:49:30 +0530

    save

--- src/pages/PrintInvoice.js
+++ src/pages/PrintInvoice.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import Layout from '../components/Layout';
